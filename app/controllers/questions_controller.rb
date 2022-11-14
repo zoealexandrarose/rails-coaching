18f1dea3c86ee845a94d5987685c536661856fa1
Zoe Rose zoealexandrarose@outlook.com <zoealexandrarose@outlook.com>
2022-11-14 12:54:46 +0100

    styling

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,16 +1,16 @@
 class QuestionsController < ApplicationController
-  def ask
-  end
+  def ask; end
 
   def answer
     @question = params[:ask]
     @answers = ["Great!", "Silly question, get dressed and go to work!", "I don't care, get dressed and go to work!"]
+
     if params[:ask] == "I am going to work"
-      @return = @answers[0]
+      @response = @answers[0]
     elsif params[:ask].end_with?
-      @return = @answers[1]
+      @response = @answers[1]
     else
-      @return = @answers[2]
+      @response = @answers[2]
     end
   end
 end
